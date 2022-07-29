8111e83955b08d557014364775d34bb2236f86c9
ted <mairurat@gmail.com>
2022-07-29 12:48:21 +0300

    Configurations

--- src/ProcessData/process.ts
+++ src/ProcessData/process.ts
@@ -7,6 +7,9 @@ config();
 //Uniswap Router v2 Address
 const uniswapAddressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
+//Token to monitor
+const tokens = process.env.TOKENS_TO_MONITOR;
+
 //INterface abstracts the encoding and decoding required to interact with contracts on eth network
 const inter = new ethers.utils.Interface(ABI);
 // console.log(inter);
@@ -21,11 +24,15 @@ export const processData = async (txContents: txContents) => {
 
             //Decode the data    
             const decode_data = inter.parseTransaction({ data: txContents.data});
-            // console.log(decode_data);
+            console.log(decode_data);
 
             //Get Method Name
             let method_name = decode_data['name'];
-            console.log(method_name);
+            // console.log(method_name);
+            //  if(method_name == "addLiquidity"){
+
+            //  }
+            
         }
     }catch(error: any){
         console.log(error.message);
